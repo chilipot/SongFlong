507d375c5ffc98cdda5ca3784fc1b446374b7aee
Daniel Guddemi <danguddemi@gmail.com>
2019-09-17 18:55:29 -0700

    Supporting chunked stream downloads

--- songflong2/download.py
+++ songflong2/download.py
@@ -1,13 +1,64 @@
 from pytube import YouTube
 from pathlib import Path
-import logging
 from time import time
-from io import BytesIO
+import multiprocessing as mp
+from math import ceil
+import requests
+import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
+
+
+def download_stream(video_url, itag, download_dir):
+    """
+    Downloads the stream by separating it into multiple chunks.
+
+    :param video_url: The YouTube link
+    :type video_url: str
+    :param itag: YouTube's stream format code
+    :type itag: int
+    :param download_dir: The Path of the download directory
+    :type download_dir: Path
+    """
+
+    CHUNK_SIZE = 3 * 2**20  # bytes
+    stream = YouTube(video_url).streams.get_by_itag(itag)
+    filename = download_dir / f"{stream.type}-{stream.default_filename}.mp4"
+    url = stream.url
+    filesize = stream.filesize
+
+    ranges = [[url, i * CHUNK_SIZE, (i + 1) * CHUNK_SIZE - 1]
+              for i in range(ceil(filesize / CHUNK_SIZE))]
+    # Last range must be to the end of file, so it will be marked as None.
+    ranges[-1][2] = None
+
+    pool = mp.Pool(min(len(ranges), 64))
+    chunks = pool.map(download_chunk, ranges)
+
+    with open(filename, 'wb') as outfile:
+        for chunk in chunks:
+            outfile.write(chunk)
+
+
+def download_chunk(args):
+    """
+    Requests a chunk of the file to be downloaded.
+
+    :param args: The chunk start and finish
+    :type args: tuple
+    :returns: The data from the get request
+    """
+    url, start, finish = args
+    range_string = '{}-'.format(start)
+
+    if finish is not None:
+        range_string += str(finish)
+
+    response = requests.get(url, headers={'Range': 'bytes=' + range_string})
+    return response.content
 
 
 def download_audio_stream(url: str, download_dir: Path) -> Path:
@@ -22,17 +73,10 @@ def download_audio_stream(url: str, download_dir: Path) -> Path:
     :rtype: Path
     """
 
-    def bitrate(stream):
-        return stream.abr in ['128kbps', '80kbps', '40kbps']
-
-    youtube = YouTube(url)
-    audio_stream = youtube.streams.filter(only_audio=True, subtype='mp4', adaptive=True, custom_filter_functions=[
-                                          bitrate]).order_by('abr').desc().first()
-
-    if audio_stream:
+    try:
         logger.info(f"Downloading audio stream -> {url}")
-        return audio_stream.download(output_path=download_dir, filename=audio_stream.type + audio_stream.default_filename)
-    else:
+        return download_stream(url, 140, download_dir)
+    except:
         logger.error(f"Unable to find an audio stream for {url}")
 
 
@@ -48,17 +92,10 @@ def download_video_stream(url: str, download_dir: Path) -> Path:
     :rtype: Path
     """
 
-    def resolutions(stream):
-        return stream.resolution in ['720p', '1080p', '480p', '360p', '240p', '144p']
-
-    youtube = YouTube(url)
-    video_stream = youtube.streams.filter(only_video=True, subtype='mp4', adaptive=True, custom_filter_functions=[
-                                          resolutions]).order_by('resolution').desc().first()
-
-    if video_stream:
+    try:
         logger.info(f"Downloading video stream -> {url}")
-        return video_stream.download(output_path=download_dir, filename=video_stream.type + video_stream.default_filename)
-    else:
+        return download_stream(url, 135, download_dir)
+    except:
         logger.error(f"Unable to find an video stream for {url}")
 
 
