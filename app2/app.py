df9a2efce34432291ba97f139716f1077962355e
Daniel Guddemi <danguddemi@gmail.com>
2019-09-30 11:03:51 -0700

    Removing testing code

--- app2/app.py
+++ app2/app.py
@@ -3,8 +3,6 @@ from redis import StrictRedis
 from rq import Queue
 from songflong2.runner import setup_download_dir, video_links, download_video_stream, generate_videos
 
-from random import randrange
-
 
 app = Flask(__name__)
 
@@ -12,17 +10,9 @@ q = Queue(connection=StrictRedis())
 
 
 @app.route('/')
-def get_randrange():
+def index():
 
-    if 'stop' in request.args:
-
-        stop = int(request.args.get('stop'))
-        start = int(request.args.get('start', 0))
-        step = int(request.args.get('step', 1))
-
-        job = q.enqueue(randrange, start, stop, step, result_ttl=5000)
-
-        return jsonify(job_id=job.get_id())
+    return "Hello, World!", 200
 
     return 'Stop value not specified!', 400
 
@@ -38,22 +28,6 @@ def generate_videos(song_name):
 
     return jsonify(job_ids=jobs)
 
-@app.route("/results")
-@app.route("/results/<string:job_id>")
-def get_results(job_id=None):
-
-    if job_id is None:
-        return jsonify(queued_job_ids=q.job_ids)
-
-    job = q.fetch_job(job_id)
-
-    if job.is_failed:
-        return 'Job has failed!', 400
-
-    if job.is_finished:
-        return jsonify(result=job.result)
-
-    return 'Job has not finished!', 202
 
 if __name__ == '__main__':
     # Start server
