ac5597bde07f0493c356eb18bdff709d425fa80c
Unix-Code <dmalakh@comcast.net>
2020-09-14 11:18:40 -0400

    add comments + fix some issues in routes

--- app/routes.py
+++ app/routes.py
@@ -10,13 +10,13 @@ def ping():
     return "pong", 200
 
 
-@JOBS.route('/submit/<string:song_name>')
+@JOBS.route('/submit/<string:song_name>', methods=['POST'])
 def submit(song_name):
-    jobs = JobService.init_jobs(song_name)
+    jobs = JobService.init_jobs(song_name)  # TODO: Actually Fix this
     if jobs:
-        return jsonify(job_ids=jobs)
+        return jsonify(job_ids=jobs), 201
     else:
-        return {"status", 'Unable to find video stream!'}, 424
+        return {'status', 'Unable to find video stream!'}, 424
 
 
 @JOBS.route('/results/<string:job_id>')
@@ -24,12 +24,12 @@ def results(job_id):
     job = JobService.get_job(job_id)
 
     if job.is_failed:
-        return {"status": 'Job has failed!'}, 400
+        return {'status': 'Job has failed!'}, 400
 
     if job.is_finished:
         return jsonify(job.result)
 
-    return {"status": 'Job has not finished!'}, 202
+    return {'status': 'Job has not finished!'}, 202
 
 
 @JOBS.route('/video/<string:video_path>')
